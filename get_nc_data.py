5e6e2f13415f77ebfffe78bdef27192e5e5cea9d
agrofunk <denis@seca.space>
2024-02-15 09:06:44 -0300

    try xt ivs

--- get_nc_data.py
+++ get_nc_data.py
@@ -8,13 +8,14 @@ from glob import glob
 sys.path.append('/home/jovyan/PlanetaryComputerExamples/CODE/pcgrits/')
 from grits import *
 from xrspatial import zonal_stats
-#from dask_gateway import GatewayCluster
+# from dask_gateway import GatewayCluster
 
 #%%
 # cluster = GatewayCluster()  
 # client = cluster.get_client()
 # cluster.adapt(minimum=4, maximum=24)
 # print(cluster.dashboard_link)
+
 # %% Area Of Interest
 # name embrapa_sc , layer talhoes, fazenda_embrapa.gpkg, column 'tid'
 path_nc = '/home/jovyan/PlanetaryComputerExamples/OUT/nc/iacanga/'
@@ -31,49 +32,45 @@ field = gpd.read_file(file, layer=layer)
 print(field.dtypes)
 
 #%%
+band = 'NDVI'
 
-r = 100
-band = 'lst'
+for r in [100,200,300,400]:
 
-field_ = field[field[column] == r ]
+    field_ = field[field[column] == r ]
 
-bbox, lat_range, lon_range = get_lims(field_)
-print(field_.head())
-field_.plot(column=attribute_col, legend=True)
+    bbox, lat_range, lon_range = get_lims(field_)
+    print(field_.head())
+    field_.plot(column=attribute_col, legend=True)
+    plt.show();plt.close()
 
+    files = sorted(glob(path_nc+'*_'+str(r)+'.nc'))
+    print(files)
 
-#%%
-files = sorted(glob(path_nc+'*_'+str(r)+'_LST_*'))
-print(files)
+    for f in files:
+        dst = xr.open_dataset(f, chunks=64)
+        fz = xr_rasterize(field_,dst,attribute_col='TID',verbose=True)
+        tempo = pd.to_datetime(dst.time.values)
+        fz = fz.chunk(64)
 
-
-#%%
-for f in files:
-    dst = xr.open_dataset(f, chunks=64)
-    fz = xr_rasterize(field_,dst,attribute_col='TID',verbose=True)
-    tempo = pd.to_datetime(dst.time.values)
-    fz = fz.chunk(64)
-
-    # extraindo primeira data
-    data_ = dst.sel(time=tempo[0]).squeeze()
-    print(f'computing stats for {tempo[0]}')
-    outst = zonal_stats(zones=fz, values=data_[band]).compute()
-    data_.close()
-    
-    # the whole thing
-    for t in tempo[1:]:
-        data_ = dst[band].sel(time=t).squeeze()
-        print(f'computing stats for {t}')
-        
-        outst1 = zonal_stats(zones=fz, values=data_).compute()
-        outst1['date'] = str(t)
-        outst = pd.concat([outst,outst1])
+        # extraindo primeira data
+        data_ = dst.sel(time=tempo[0]).squeeze()
+        print(f'computing stats for {tempo[0]}')
+        outst = zonal_stats(zones=fz, values=data_[band]).compute()
         data_.close()
-        del outst1
+        
+        # the whole thing
+        for t in tempo[1:]:
+            data_ = dst[band].sel(time=t).squeeze()
+            print(f'computing stats for {t}')
+            outst1 = zonal_stats(zones=fz, values=data_).compute()
+            outst1['date'] = str(t)
+            outst = pd.concat([outst,outst1])
+            data_.close()
+            del outst1
 
-    d0 = f.split('/')[-1].split('_')[0]
-    d1 = f.split('/')[-1].split('_')[1]
-    outst.to_csv(f'{path_csv}/{r}_{d0}_{d1}_{band}.csv')
-    print(f'{path_csv}/{r}_{d0}_{d1}_{band}.csv \n')
+        d0 = f.split('/')[-1].split('_')[0]
+        d1 = f.split('/')[-1].split('_')[1]
+        outst.to_csv(f'{path_csv}/{r}_{d0}_{d1}_{band}.csv')
+        print(f'{path_csv}/{r}_{d0}_{d1}_{band}.csv \n')
 
     # %%
